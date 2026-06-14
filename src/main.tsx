41346387790b2e224b3b02bbb7c19dc26ea27e45
jonny720 <jonnyh360@gmail.com>
2026-06-14 20:21:47 +0300

    feat: add @vercel/analytics dependency and integrate analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { LanguageProvider } from './hooks/useLanguage';
 import App from './app/App';
 import './styles/index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <Analytics />
     </LanguageProvider>
   </StrictMode>
 );
